eba45669e04810acb5f44f3eaa88f05bc0441756
catapillar0505 <twrp3301@gmail.com>
2024-08-03 01:37:48 +0900

    feature/화면구성

--- lib/screens/sign_up_screen.dart
+++ lib/screens/sign_up_screen.dart
@@ -6,35 +6,65 @@ class SignUpScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            // Image.asset(
-            //   'assets/app_icon.png', // 앱 아이콘 경로
-            //   height: 100,
-            // ),
-            SizedBox(height: 20),
-            Text(
-              '동백',
-              style: TextStyle(
-                fontSize: 24,
-                color: Colors.green,
+        child: Padding(
+          //좌우 여백-> padding 위젯 추가
+        padding: const EdgeInsets.symmetric(horizontal: 50.0), // 왼쪽과 오른쪽 여백 50px 추가
+          child: Column(
+            children: [
+              SizedBox(height: 100), // 상하 여백
+              Image.asset(
+                'assets/app_icon.png',
+                height: 90   ,
               ),
-            ),
-            SizedBox(height: 40),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/profilePrompt');
-              },
-              child: Text('회원가입'),
-            ),
-            TextButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/positionSelection');
-              },
-              child: Text('회원가입 없이 둘러보기'),
-            ),
-          ],
+              //카카오 소셜로그인
+              SizedBox(height: 100),
+              ElevatedButton(
+                onPressed: () {
+                  // 버튼 클릭 시 동작 정의
+                  Navigator.pushNamed(context, '/profilePrompt');
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color(0xFFFEE500), // 버튼 배경색
+                  foregroundColor: Colors.black, // 버튼 텍스트 색깔
+                  padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 16.0), // 수평 패딩 설정
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12), // 버튼 모서리 반경
+                  ),
+                ),
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Image.asset(
+                      'assets/kakao_symbol.png',
+                      height: 24.0, // 심볼의 높이 설정
+                      width: 24.0, // 심볼의 너비 설정
+                    ),
+                    SizedBox(width: 5.0), // 이미지와 텍스트 사이 간격
+                    Text(
+                      '카카오 계정으로 로그인하기',
+                      style: TextStyle(
+                        fontSize: 16.0, // 글자 크기 설정
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 200),
+              TextButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/positionSelection');
+                },
+                style: TextButton.styleFrom(
+                  foregroundColor: Color(0xFF908C8C), // 텍스트 색깔 변경
+                ),
+                child: Text('회원가입 없이 둘러보기',
+                  style: TextStyle(
+                    fontSize: 16.0, // 글자 크기 설정
+                  ),),
+              ),
+            ],
+          ),
         ),
       ),
     );
